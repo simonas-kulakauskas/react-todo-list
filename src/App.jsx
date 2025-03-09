06227a20a8bd3e2f37cd38c7940ac5139a467ba5
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-09 23:42:27 +0000

    Added input boxes and lables for input As well as function for it and a State

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import { todoListItems } from './data';
 
 
 function App() {
+  const [newTodoListItems, setNewTodoListItems] = useState(todoListItems);
+
+  function handleClick() {
+    alert("I've been clicked!");
+  }
+
   return (
     <>
       <h1>To Do List: </h1>
+
+      <label for="newItemInput">New Note: </label>
+      <input id="newItemInput" type="text" placeholder='Click add...'></input>
+      <button id="addItem" style={{marginLeft: 5}} onClick={handleClick}>ADD</button>
+
       <ul>
-        {todoListItems.map((item) => { // Iterating through all items.
+        {newTodoListItems.map((item) => { // Iterating through all items.
           return (
             item.checkedOff ? <li key={item.key}><s>{item.itemValue}</s></li> : <li key={item.key}>{item.itemValue}</li> // Check off if needed.
           );
