48ad1c8d27e158998f7433c1d921d6af5a17dc45
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-09 23:58:03 +0000

    Changed up format, removed the rendering logic from App() component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,32 @@ import './App.css'
 import { todoListItems } from './data';
 
 
+function DisplayTodoList({ currentListItems }) { 
+  return (
+    <ul>
+      {currentListItems.map((item) => { // Iterating through all items.
+        return (
+          item.checkedOff ? <li key={item.key}><s>{item.itemValue}</s></li> : <li key={item.key}>{item.itemValue}</li> // Check off if needed.
+        );
+      })}
+    </ul>
+  ); 
+}
+
+function AddToList() {
+
+}
+
 function App() {
-  const [newTodoListItems, setNewTodoListItems] = useState(todoListItems);
-
-  function handleClick() {
-    alert("I've been clicked!");
-  }
-
   return (
     <>
       <h1>To Do List: </h1>
 
-      <label for="newItemInput">New Note: </label>
-      <input id="newItemInput" type="text" placeholder='Click add...'></input>
-      <button id="addItem" style={{marginLeft: 5}} onClick={handleClick}>ADD</button>
+      <form>
 
-      <ul>
-        {newTodoListItems.map((item) => { // Iterating through all items.
-          return (
-            item.checkedOff ? <li key={item.key}><s>{item.itemValue}</s></li> : <li key={item.key}>{item.itemValue}</li> // Check off if needed.
-          );
-        })}
-      </ul>
+      </form>
+      
+      <DisplayTodoList currentListItems={todoListItems}/>
     </>
   );
 }
