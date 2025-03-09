4e99f1461bf2e48b24862fcbee704b47994ea4fc
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-09 22:44:55 +0000

    Created a example data set and example of how it should be listed / look.

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export let todoListItems = [
+    {
+        key: 0,
+        itemValue: "Hello!",
+        checkedOff: true
+    },
+    {
+        key: 1,
+        itemValue: "Hey!",
+        checkedOff: false
+    }
+];
