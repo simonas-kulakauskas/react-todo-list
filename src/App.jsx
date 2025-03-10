a1aeced5972ee872d2065197fbd410e686ba9e2e
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-10 00:27:19 +0000

    Reformatted and got it functionally working - Still need to implement the 'checkedOff' functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { todoListItems } from './data';
 
 
-function DisplayTodoList({ currentListItems }) { 
+function RenderListItems({ currentListItems }) { /* Render's the items of the list*/
   return (
     <ul>
       {currentListItems.map((item) => { // Iterating through all items.
@@ -15,28 +15,40 @@ function DisplayTodoList({ currentListItems }) {
   ); 
 }
 
-function DisplayListAddForm() {
-  function handleClick() {
-    alert("I've been pressed!")
+function RenderTodoForm() { /* Renders the list and holds it's logic */
+    const [currentListItems, setCurrentListItems] = useState(todoListItems);
+  
+    function handleClick(value) {
+      setCurrentListItems([
+        ...currentListItems,
+        {
+          key: currentListItems.length + 1,
+          itemValue: value,
+          checkedOff: false
+        }
+      ])
+    }
+  
+    return (
+      <>
+        <form>
+              <label htmlFor='itemInput'>New Item: </label>
+              <input type='text' id='itemInput' name='itemInput' placeholder='Enter here...'></input>
+              <button onClick={e => {
+                e.preventDefault(); 
+                handleClick(document.getElementById("itemInput").value)
+                }}>Add</button>
+        </form>
+        <RenderListItems currentListItems={currentListItems}/>
+      </>
+    );
   }
-
-  return (
-    <form>
-          <label htmlFor=''>New Item: </label>
-          <input type='text' id='itemInput' name='itemInput' placeholder='Enter here...'></input>
-          <button onClick={handleClick}>Add</button>
-    </form>
-  );
-}
 
 function App() {
   return (
     <>
-      <h1>To Do List: </h1>
-
-      <DisplayListAddForm />
-    
-      <DisplayTodoList currentListItems={todoListItems}/>
+      <h1>To-do List: </h1>
+      <RenderTodoForm />
     </>
   );
 }
