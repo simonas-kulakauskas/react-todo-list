5beb1551c6b1e0d00716657fa18dd3759ba71bbc
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-11 16:23:49 +0000

    Comment change to add new task

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useEffect } from 'react';
 import { useLocalStorage } from 'usehooks-ts'
 import './App.css'
 
-/* ✅/❌
+/* ✅/
 * 1. Get data from 'data.js' and render as list items on unordered list.              [✅] 
 * 2. Add the ability to add items to our own state list.                              [✅] 
 * 3. Add checkmarks and ability to check / cross-out tasks that have been completed.  [✅]     
 * 4. Add the ability to save to-do items locally.                                     [✅] 
+* 5. Individual delete buttons for items [❌]
 */
 
 function App() {
